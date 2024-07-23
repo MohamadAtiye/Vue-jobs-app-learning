f70bb40f332250307eb077d92d548d301b4c77e5
Mohamad Atiye <mohamad.atiye@gmail.com>
2024-07-23 15:45:23 +0300

    init push

---
+++ src/App3.vue
@@ -0,0 +1,53 @@
+<script>
+import { ref } from "vue"
+export default {
+  setup() {
+    const name = ref("Mahdi");
+    const status = ref("active");
+    const tasks = ref(['t1', 't2', 't3'])
+
+    const toggleStatus = () => {
+      if (status.value === 'active') status.value = "pending";
+      else if (status.value === 'pending') status.value = "inactive";
+      else status.value = "active";
+    }
+
+    return {
+      name, status, tasks, toggleStatus
+    }
+  }
+  // data() {
+  //   return {
+  //     name: "mahdi",
+  //     status: 'other',
+  //     tasks: ['t1', 't2', 't3'],
+  //     link: 'https://google.com'
+  //   }
+  // },
+  // methods: {
+  //   toggleStatus() {
+  //     if (this.status === 'active') this.status = "pending";
+  //     else if (this.status === 'pending') this.status = "inactive";
+  //     else this.status = "active";
+  //   }
+  // }
+}
+</script>
+
+<template>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is Inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <!-- <a v-bind:href="link">Link</a> -->
+  <a :href="link">Link</a>
+  <br />
+  <!-- <button v-on:click="toggleStatus">Toggle Status</button> -->
+  <button @click="toggleStatus">Toggle Status</button>
+</template>
---
+++ src/App4.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, onMounted } from "vue"
+const name = ref("Mahdi");
+const status = ref("active");
+const tasks = ref(['t1', 't2', 't3'])
+
+const newTask = ref('')
+
+const toggleStatus = () => {
+  if (status.value === 'active') status.value = "pending";
+  else if (status.value === 'pending') status.value = "inactive";
+  else status.value = "active";
+}
+
+const addTask = () => {
+  if (newTask.value.trim !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map(t => t.title)
+
+  } catch (e) {
+    console.log("error fetching", e)
+  }
+})
+</script>
+
+<template>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is Inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="(task, index,) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
+  </ul>
+
+  <a :href="link">Link</a>
+  <br />
+  <button @click="toggleStatus">Toggle Status</button>
+</template>
